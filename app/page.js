b9e4369c05434c57ca4edc5b13c44e033a3ad72a
Tihong Chhiv <u6632088@au.edu>
2025-09-27 21:59:35 +0700

    initial commit

--- app/page.js
+++ app/page.js
@@ -6,12 +6,12 @@ export default function BoxBasic() {
   return (
     <main>
       <Box component="section" className="border border-gray-800 m-5 text-center">
-        <h1 className="text-3xl text-violet-950">Stock Management v1.0</h1>
+        <h1>Stock Management v1.0</h1>
         <ul>
-          <li><a href="/product">Products</a></li>
-          <li><a href="/category">Category</a></li>
+          <li><a href="/app/stock/product">Products</a></li>
+          <li><a href="/app/stock/category">Category</a></li>
         </ul>
-        
+
       </Box>
     </main>
   );
